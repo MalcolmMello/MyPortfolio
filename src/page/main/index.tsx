afac652402f1adb34f579f3a5e5baffd0b6446e0
MalcolmMello <malcolmmellolima@gmail.com>
2022-01-20 20:48:28 -0300

    scroll

---
+++ src/page/main/index.tsx
@@ -0,0 +1,92 @@
+import * as C from './styles'
+import { Header } from '../../components/Header'
+import { MyData } from '../../components/MyData'
+import { About } from '../../components/About'
+import { Skills } from '../../components/Skills'
+import { Projects } from '../../components/MyProjects'
+import { Services } from '../../components/MyServices'
+import { Contact } from '../../components/Contact'
+import { Footer } from '../../components/Footer'
+import { useRef } from 'react'
+
+export const Main = () => {
+    const aboutSection: any = useRef(null)
+    const skillsSection: any = useRef(null)
+    const projectsSection: any = useRef(null)
+    const servicesSection: any = useRef(null)
+    const contactSection: any = useRef(null)
+
+    
+    const goToAbout = () => {
+        window.scrollTo({
+            top: aboutSection.current.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+
+    const goToSkills = () => {
+        window.scrollTo({
+            top: skillsSection.current.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+
+    const goToProjects = () => {
+        window.scrollTo({
+            top: projectsSection.current.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+
+    const goToServices = () => {
+        window.scrollTo({
+            top: servicesSection.current.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+
+    const goToContacts = () => {
+        window.scrollTo({
+            top: contactSection.current.offsetTop,
+            behavior: 'smooth'
+        })
+    }
+    
+    return (
+        <C.MainContent>
+			<header>
+                <div className='container-header'>
+                    <h1>Malcolm Lima</h1>
+                    <ul>
+                        <li onClick={goToAbout}>Sobre mim</li>
+                        <li onClick={goToSkills}>Skills</li>
+                        <li onClick={goToProjects}>Projetos</li>
+                        <li onClick={goToServices}>Serviços</li>
+                        <li onClick={goToContacts}>Contato</li>
+                    </ul>
+                </div>
+            </header>
+			<main>
+                <div >
+                    <MyData />
+                </div>
+                <div ref={aboutSection}>
+                    <About />
+                </div>
+                <div ref={skillsSection}>
+                    <Skills />
+                </div>
+                <div ref={projectsSection}>
+				    <Projects />
+                </div>
+                <div ref={servicesSection}>
+                    <Services />    
+                </div>
+                <div ref={contactSection}>
+                    <Contact />
+                </div>
+			</main>
+			<Footer />
+		</C.MainContent>
+    )
+}
---
+++ src/page/main/styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const MainContent = styled.div`
+    .container-header {
+        display: flex;
+        width: 1024px;
+        height: 90px;
+        justify-content: space-between;
+        align-items: center;
+        margin: auto;
+        color: #FFF;
+
+        ul {
+            display: flex;
+            list-style-type: none;
+
+            li {
+                margin-right: 50px;
+                cursor: pointer;
+                border-bottom: 2px solid transparent;
+                padding: 10px 0px;
+                transition: all ease 0.3s;
+            }
+            li:hover {
+                border-bottom: 1px solid #5CC4C4
+            }
+        }
+    }
+`
